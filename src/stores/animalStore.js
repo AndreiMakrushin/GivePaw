5b7420d72a0af63bea97454df566c70113bfca17
Andrei <andrei18makrushin@gmail.com>
2023-05-20 01:17:13 +0500

    strapi

--- src/stores/animalStore.js
+++ src/stores/animalStore.js
@@ -5,11 +5,10 @@ import axios from "axios";
 export const useAnimalStore = defineStore("animals", () => {
   const openModal = ref(false);
   const getAnimals = async() => {
-    const response = await axios.get('http://localhost:1337/api/animals')
+   const responce = await axios.get('http://localhost:1337/api/animals?populate=*')
+   return responce.data
+  }
 
-    console.log(response);
-  };
-  getAnimals()
   const setAnimals = () => {};
   return { getAnimals, setAnimals, openModal };
 });
--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -1,8 +1,29 @@
+<script setup>
+    import { ref, onMounted } from "vue";
+    import { useAnimalStore } from "../stores/animalStore";
+
+    const animals = useAnimalStore()
+    const result = ref(null)
+
+    onMounted(async()=>{
+        const animal = await animals.getAnimals()
+        result.value = animal.data
+    })
+</script>
 <template>
     <div>
         <div class="catalog-container">
-            <div class="container">
-                <h1>В разработке!</h1>
+            <div class="container" v-for="oneAnimal in result" :key="oneAnimal.id">
+                <h2>{{oneAnimal.attributes.name}}</h2>
+                <h2>{{oneAnimal.attributes.breed}}</h2>
+                <h2>{{oneAnimal.attributes.size}}</h2>
+                <h2>{{oneAnimal.attributes.descriptions}}</h2>
+                <h2>{{oneAnimal.attributes.overexposure}}</h2>
+                <h2>{{oneAnimal.attributes.gender}}</h2>
+                <img :src="`http://localhost:1337${oneAnimal.attributes.img.data[0].attributes.url}`">
+                <h2>{{oneAnimal.attributes.type}}</h2>
+                <h2>{{oneAnimal.attributes.date}}</h2>
+                <h2>{{oneAnimal.attributes.region}}</h2>
             </div>
         </div>
     </div>
